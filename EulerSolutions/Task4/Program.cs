f0f36faa9e33622085a11cf81789da2306f52a42
qwertyspb <alexander.razzhivin.95@gmail.com>
2023-03-19 20:30:34 +0300

    added Task4 and Task5

---
+++ EulerSolutions/Task4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.InteropServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var input = 1000;
+        var results = new List<int>();
+
+        for (var x = 100; x < input; x++)
+        {
+            for (var y = 100; y < input; y++)
+            {
+                var result = x * y;
+
+                if (IsPalindromic(result))
+                {
+                    results.Add(result);
+                }
+            }
+        }
+
+        Console.WriteLine(results.Max());
+    }
+
+    private static bool IsPalindromic(int value)
+    {
+        var str = value.ToString();
+        var reversed = string.Join(string.Empty, str.Reverse());
+
+        return str.Equals(reversed);
+    }
+}
---
+++ EulerSolutions/Task5/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var till = 20;
+        const int Limit = 1000000000;
+
+        for (var i = till; i < Limit; i++)
+        {
+            for (var x = 1; x <= till; x++)
+            {
+                var reminder = i % x;
+
+                if (reminder == 0)
+                {
+                    if (x == till)
+                    {
+                        Console.WriteLine($"The smallest positive number that is evenly divisible by all of the numbers from 1 to {till} is {i}");
+                        return;
+                    }
+                }
+
+                else
+                    break;
+            }
+        }
+    }
+}
