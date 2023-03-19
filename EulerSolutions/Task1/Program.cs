fed4a0f69f83d20bb2979a0b551f4a5d83674772
qwertyspb <alexander.razzhivin.95@gmail.com>
2023-03-19 13:30:33 +0300

    added Task1 logic

---
+++ EulerSolutions/Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var nums = Enumerable.Range(1, 9);
+
+        var dividers = new List<int>() { 3, 5, 2};
+
+        nums = GetAllMultiplesOf(dividers, nums);
+
+        foreach (var i in nums)
+        {
+            Console.Write($"{i}; ");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine($"Sum of the multiples: {nums.Sum()}");
+    }
+
+    private static IEnumerable<int> GetAllMultiplesOf(IEnumerable<int> dividers, IEnumerable<int> arr)
+    {
+        var lists = new List<List<int>>();
+
+        foreach (var d in dividers) 
+            lists.Add(arr.Where(x => x % d == 0).ToList());
+
+        IEnumerable<int> result = new List<int>();
+
+        for (var i = 0; i < lists.Count; i++)
+        {
+            result = result.Union(lists[i]);
+        }
+
+        return result;
+    }
+}
