58c001581770fd9e6918544b714719de72a3d743
qwertyspb <alexander.razzhivin.95@gmail.com>
2023-03-21 22:02:51 +0300

    added Task12

---
+++ EulerSolutions/Task12/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Net.Http.Headers;
+
+internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+        const int Aim = 501;
+        long x = 0;
+        long sum = 0;
+        int value = 0;
+
+        while (!HasEnoughDivisors(sum, Aim, out var newValue))
+        {
+            if (newValue > value)
+            {
+                value = newValue;
+                Console.WriteLine($"Current maximum number of found divisors: {value}");
+            }
+
+            x++;
+            sum += x;
+        }
+
+        Console.WriteLine(sum);
+    }
+
+    private static bool HasEnoughDivisors(long sum, int aim, out int divisorNumber)
+    {
+        if (sum % (2 * 3) != 0)
+        {
+            divisorNumber = 0;
+            return false;
+        }
+
+        var list = new List<long>();
+
+        for (long i = 1; i <= sum; i++)
+        {
+            if (sum % i == 0)
+                list.Add(i);
+
+            if (list.Count == aim)
+            {
+                divisorNumber = list.Count;
+                return true;
+            }
+        }
+        
+        divisorNumber = list.Count;
+
+        return false;
+    }
+}
