a1b046f7ad45f0cb3eda675ac83444ebba0f9684
qwertyspb <alexander.razzhivin.95@gmail.com>
2023-04-12 17:30:04 +0600

    added Task16

---
+++ EulerSolutions/Task16/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        const int Pow = 15;
+
+        var income = Math.Pow(2, Pow);
+
+        var arr = income.ToString().ToCharArray();
+
+        var nums = new List<double>();
+
+        foreach (var c in arr)
+        {
+            double.TryParse(c.ToString(), out var result);
+            nums.Add(result);
+        }
+
+        Console.WriteLine(nums.Sum());
+    }
+}
