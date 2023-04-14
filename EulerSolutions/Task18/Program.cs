256f2974674f51efe77c212598a5de85d04f89aa
qwertyspb <alexander.razzhivin.95@gmail.com>
2023-04-14 14:45:40 +0600

    added Task18

---
+++ EulerSolutions/Task18/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // I misunderstood the requirements and when I finally got it, I was sick and tired and wasn't able to fix it :)
+
+        var input = "75\r\n95 64\r\n17 47 82\r\n18 35 87 10\r\n20 04 82 47 65\r\n19 01 23 75 03 34\r\n88 02 77 73 07 63 67\r\n99 65 04 28 06 16 70 92\r\n41 41 26 56 83 40 80 70 33\r\n41 48 72 33 47 32 37 16 94 29\r\n53 71 44 65 25 43 91 52 97 51 14\r\n70 11 33 28 77 73 17 78 39 68 17 57\r\n91 71 52 38 17 14 91 43 58 50 27 29 48\r\n63 66 04 68 89 53 67 30 73 16 69 87 40 31\r\n04 62 98 27 23 09 70 98 73 93 38 53 60 04 23";
+
+        var inputAsList = input.Split("\r\n");
+
+        var intList = inputAsList.Select(str =>
+        {
+            var list = str.Split(' ');
+            return list.Select(x => decimal.Parse(x)).ToList();
+        })
+        .ToList();
+
+        var sum = 0m;
+        var place = 0;
+        foreach ( var list in intList )
+        {
+            try
+            {
+                place = list[place] > list[place + 1]
+                ? place
+                : place + 1;
+            }
+            catch (ArgumentOutOfRangeException) {}
+
+            sum += list[place];
+        }
+
+        Console.WriteLine();
+        Console.WriteLine(sum);
+    }
+}
