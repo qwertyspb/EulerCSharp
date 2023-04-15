302ac81bd7b3e6698d108cf82aadf54d980171da
qwertyspb <alexander.razzhivin.95@gmail.com>
2023-04-15 14:40:41 +0600

    added Task22

---
+++ EulerSolutions/Task22/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Xml.Linq;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+
+        var income = File.ReadAllText(@"C:\Users\alexa\PracticeCSharp\p022_names.txt");
+
+        var names = income
+            .Split(',')
+            .Select(x => x.Replace("\"", string.Empty))
+            .ToList();
+        names.Sort();
+
+        var result = new List<decimal>();
+
+        names.ForEach(name =>
+        {
+            var arr = name.ToCharArray();
+
+            var letterSum = arr.Select(letter => Array.IndexOf(alphabet, letter) + 1)
+                .Sum();
+
+            var place = names.IndexOf(name) + 1;
+
+            result.Add(letterSum * place);
+        });
+
+        Console.WriteLine(result.Sum());
+    }
+}
