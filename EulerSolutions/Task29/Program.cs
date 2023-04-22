c21870e59ce83a275ce6bf58edb00a17a2df62b5
qwertyspb <alexander.razzhivin.95@gmail.com>
2023-04-22 12:53:09 +0600

    added Task29

---
+++ EulerSolutions/Task29/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var range = Enumerable.Range(2, 99);
+        var list = new List<double>();
+
+        foreach (var i in range)
+            foreach (var j in range)
+                list.Add(Math.Pow(i, j));
+
+        list = list.Distinct()
+            .OrderBy(x => x)
+            .ToList();
+
+        Console.WriteLine($"Count: {list.Count}");
+    }
+}
